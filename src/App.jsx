fb6fa26cec4ead12fe7bce84bacc07ff2fde0436
sagar.shinde <sagar.shinde@infogain.com>
2025-01-23 16:14:08 +0000

    revert changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <h1>Github Actions</h1>
-      <h2>Complete course with 2 projects for github actions</h2>
+      <h2>Complete course with 2 projects</h2>
     </>
   )
 }
